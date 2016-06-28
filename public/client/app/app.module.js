d1be7359fc24c90c97c14ed2c3af9227480a620c
Brandon Stookey <bwstookey@gmail.com>
2016-06-27 22:57:41 -0400

    dates and time parsed correctly

--- public/client/app/app.module.js
+++ public/client/app/app.module.js
@@ -2,16 +2,11 @@ angular.module('project.services', [])
 
 .factory('projectFactory', ['$http', function($http){
 
-	var getCar = function(){
+	var getCar = function(dest, startDate, endDate, pickUpTime, dropOffTime){
 		console.log('Inside of getCar dude!');
 		return $http({
 			method: 'GET',
-			url: 'http://api.hotwire.com/v1/search/car?apikey=qqvyj5fw2sbye5upjv7pu6c8',
-			// url: 'https://www.reddit.com/.json',
-			// url: 'http://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&mode=json&units=imperial&APPID=93b03ba103c6cabec96f47a6f650769e',
-			// headers: {
-   //          'Access-Control-Allow-Origin': 'http://localhost:8080',
-   //    },
+			url: 'http://api.hotwire.com/v1/search/car?apikey=qqvyj5fw2sbye5upjv7pu6c8&dest=' + dest +'&startdate='+ startDate + '&enddate='+ endDate +'&pickuptime='+ pickUpTime + '&dropofftime='+ dropOffTime ,
 		}).then(function(result) {
 			console.log('results: ', result);
       return result.data;
--- public/client/app/shared/homeView/homeViewController.js
+++ public/client/app/shared/homeView/homeViewController.js
@@ -5,4 +5,47 @@ angular.module('project.homeView', [])
 .controller('homeViewController', ['$scope','projectFactory', function($scope, projectFactory){
 	projectFactory.getCar();
 	console.log('I am homeViewController!');
+
+	$scope.catchDetails = function(dest, startDate, endDate, pickUpTime, dropOffTime){
+
+		//======================================User Selects Pick Up Time
+		var pickUpTimeMinutes = pickUpTime.getMinutes();
+		var pickUpTimeHour = pickUpTime.getHours();
+
+		pickUpTimeHour = pickUpTimeHour < 10 ? '0'+ pickUpTimeHour : pickUpTimeHour;
+		pickUpTimeMinutes = pickUpTimeMinutes < 10 ? '0'+ pickUpTimeMinutes : pickUpTimeMinutes;
+		
+		console.log('pickUpTime: ', pickUpTimeHour + ":" + pickUpTimeMinutes);
+
+		//=======================================User Selects Pick Up date
+
+		var startDateMonth = startDate.getMonth() + 1 
+		var startDateDay = startDate.getDate()
+
+		startDateMonth = startDateMonth < 10 ? '0'+ startDateMonth : startDateMonth;
+		startDateDay = startDateDay < 10 ? '0'+ startDateDay : startDateDay;
+
+		console.log('pickUpDate: ', startDateMonth + "/" + startDateDay + "/" + startDate.getFullYear());
+
+		//=====================================User Selects Drop Off Time
+		var dropOffTimeMinutes = dropOffTime.getMinutes();
+		var dropOffTimeHour = dropOffTime.getHours();
+
+		dropOffTimeHour = dropOffTimeHour < 10 ? '0'+ dropOffTimeHour : dropOffTimeHour;
+		dropOffTimeMinutes = dropOffTimeMinutes < 10 ? '0'+ dropOffTimeMinutes : dropOffTimeMinutes;
+		
+		console.log('dropOffTime: ',	dropOffTimeHour + ":" + dropOffTimeMinutes);
+
+		//=======================================User Selects Drop Off Date
+
+		var endDateMonth = endDate.getMonth() + 1 
+		var endDateDay = endDate.getDate()
+
+		endDateMonth = endDateMonth < 10 ? '0'+ endDateMonth : endDateMonth;
+		endDateDay = endDateDay < 10 ? '0'+ endDateDay : endDateDay;
+
+		console.log('endDate: ', endDateMonth + "/" + endDateDay + "/" + endDate.getFullYear());
+
+
+	}
 }]);
