cab92926cb8ec20d4304813e5699e715ef892c7e
Brandon Stookey <bwstookey@gmail.com>
2016-07-01 13:41:49 -0400

    Cleaning up files

--- server/routes.js
+++
@@ -1,4 +0,0 @@
-var app = require('./server');
-var path1 = require('path');
-var public1 = path1.resolve('public') + '/';
-var helpers = require(path1.resolve('server/helpers'));
--- server/server.js
+++ server/server.js
@@ -3,16 +3,13 @@
 var express = require('express');
 var cors = require('cors');
 var morgan = require('morgan');
-var db = require('./../db/db.js');
 var bodyParser = require('body-parser');
 
 var app = module.exports = express();
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
-// app.use(cors());
-
-var routes = require('./routes');
+app.use(cors());
 
 app.use(express.static(__dirname + './../public/client'));
 
@@ -22,13 +19,3 @@ app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
 
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Access-Token, X-Key');
-//   if (req.method == 'OPTIONS') {
-//     res.status(200).end();
-//   } else {
-//     next();
-//   }
-// });
