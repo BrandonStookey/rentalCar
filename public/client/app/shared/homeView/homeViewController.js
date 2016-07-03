3b312999bee9f2fc92192f9cf62d7655cb93c943
Brandon Stookey <bwstookey@gmail.com>
2016-07-03 17:57:37 -0400

    Homepage style near complete

--- public/client/app/shared/homeView/homeViewController.js
+++ public/client/app/shared/homeView/homeViewController.js
@@ -4,7 +4,38 @@ angular.module('project.homeView', ['ui.bootstrap','angular-loading-bar', 'ngAni
 
 .controller('homeViewController', ['$scope','projectFactory', function($scope, projectFactory){
 	console.log('I am homeViewController!');
+//============================Time Selector
+  // $scope.mytime = new Date();
 
+  $scope.hstep = 1;
+  $scope.mstep = 1;
+
+  $scope.options = {
+    hstep: [1, 2, 3],
+    mstep: [1, 5, 10, 15, 25, 30]
+  };
+
+  $scope.ismeridian = true;
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+  // $scope.update = function() {
+  //   var d = new Date();
+  //   d.setHours( 14 );
+  //   d.setMinutes( 0 );
+  //   $scope.mytime = d;
+  // };
+
+  // $scope.changed = function () {
+  //   $log.log('Time changed to: ' + $scope.mytime);
+  // };
+
+  // $scope.clear = function() {
+  //   $scope.mytime = null;
+  // };
+
+//============================Date Selector
 $scope.today = function() {
     $scope.dt = new Date();
   };
