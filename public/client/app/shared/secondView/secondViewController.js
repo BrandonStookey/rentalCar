60f6ea986ea79d8710d2e4dfa16bc3154c51c561
Brandon Stookey <bwstookey@gmail.com>
2016-07-03 18:56:17 -0400

    Centered loading dial

--- public/client/app/shared/secondView/secondViewController.js
+++ public/client/app/shared/secondView/secondViewController.js
@@ -2,15 +2,18 @@
 
 angular.module('project.secondView', ['ui.bootstrap','angular-loading-bar', 'ngAnimate'])
 
-.controller('secondViewController', ['$scope','projectFactory', function($scope, projectFactory){
+.controller('secondViewController', ['$scope','projectFactory', '$location',function($scope, projectFactory, $location){
 	console.log('I am secondViewController!')
 	$scope.carSearchResult;
-
 
 	projectFactory.getCar().then(function(data){ 
 			console.log('secondView data:', data);
 			$scope.carSearchResult = data;
 			console.log('$scope.carSearchResult: ', $scope.carSearchResult);
+			if(!$scope.carSearchResult){
+				console.log('Go Home!');
+				$location.path('/homeView');
+			}
 		});
 
 }]);
